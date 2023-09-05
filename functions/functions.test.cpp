2e9264202107152ed650b3901a03dbbe8d1d35cb
silvia-vicentini <143335928+silvia-vicentini@users.noreply.github.com>
2023-09-05 12:58:59 +0200

    Update functions.test.cpp

--- functions/functions.test.cpp
+++ functions/functions.test.cpp
@@ -11,73 +11,46 @@ bool result(pf::Population p, pf::Population v) {
 }
 
 TEST_CASE("Testing the approximations") {
-  SUBCASE("Testing the solve function: case 1") {
-    pf::Population initial_population{187, 4, 0};
-    pf::Epidemic epidemic(0.6, 0.2, initial_population);
-    int N = 191;
-    auto result = epidemic.solve(initial_population);
-    CHECK(result.S == 186);
-    CHECK(result.I == 5);
-    CHECK(result.R == 1);
+  SUBCASE("For tot > N, case 1") {
+    pf::Population initial_population{997, 3, 0};
+    pf::Epidemic epidemic(0.8, 0.4, initial_population, 8);
+    const auto state = epidemic.evolve();
+    CHECK(result(state[4], {980, 13, 7}));
+    CHECK(result(state[8], {912, 46, 42}));
   }
 
-  SUBCASE("Testing the solve function: case 2") {
-    pf::Population initial_population{187, 4, 0};
-    pf::Epidemic epidemic(0.65, 0.05, initial_population);
-    int N = 191;
-    auto result = epidemic.solve(initial_population);
-    CHECK(result.S == 185);
-    CHECK(result.I == 5);
-    CHECK(result.R == 0);
+  SUBCASE("For tot > N, case 2") {
+    pf::Population initial_population{997, 3, 0};
+    pf::Epidemic epidemic(0.8, 0.27, initial_population, 25);
+    const auto state = epidemic.evolve();
+    CHECK(result(state[1], {995, 5, 0}));
+    CHECK(result(state[16], {160, 290, 550}));
+    CHECK(result(state[25], {49, 39, 912}));
   }
 
-  SUBCASE("Testing the approx function for tot > N") {
-    pf::Population initial_population{187, 4, 0};
-    pf::Epidemic epidemic(0.6, 0.2, initial_population);
-    int N = 191;
-    auto result = epidemic.approx(initial_population);
-    CHECK(result.S == 186);
-    CHECK(result.I == 5);
-    CHECK(result.R == 0);
+  SUBCASE("For tot < N, case 1") {
+    pf::Population initial_population{997, 3, 0};
+    pf::Epidemic epidemic(0.8, 0.4, initial_population, 20);
+    const auto state = epidemic.evolve();
+    CHECK(result(state[9], {878, 62, 60}));
+    CHECK(result(state[17], {365, 163, 472}));
   }
 
-  SUBCASE("Testing the approx function for tot < N") {
-    pf::Population initial_population{187, 4, 0};
-    pf::Epidemic epidemic(0.65, 0.05, initial_population);
-    int N = 191;
-    auto result = epidemic.approx(initial_population);
-    CHECK(result.S == 185);
-    CHECK(result.I == 6);
-    CHECK(result.R == 0);
+  SUBCASE("For tot < N, case 2") {
+    pf::Population initial_population{997, 3, 0};
+    pf::Epidemic epidemic(0.8, 0.27, initial_population, 25);
+    const auto state = epidemic.evolve();
+    CHECK(result(state[6], {941, 41, 18}));
+    CHECK(result(state[10], {722, 175, 103}));
+    CHECK(result(state[18], {98, 207, 695}));
   }
 }
-
-TEST_CASE("Testing the lockdown function") {
-  pf::Population initial_population{45, 210, 45};
-  pf::Epidemic epidemic(0.99, 0.01, initial_population);
-  int N = 300;
-  auto result = epidemic.lockdown(initial_population);
-  CHECK(result.S == 45);
-  CHECK(result.I == 189);
-  CHECK(result.R == 66);
-}
-/*
-TEST_CASE("Testing the vaccine function") {
-  Epidemic epidemic{0.55, 0.4};
-  Population initial_population{5076, 45, 4879};
-  int N = 10000;
-  auto result = epidemic.lockdown(initial_population, N);
-  CHECK(result.S == 4810);
-  CHECK(result.I == 40);
-  CHECK(result.R == 5150);
-}*/
 
 TEST_CASE("Testing the evolve function") {
   SUBCASE("Normal use, beta -> gamma, N=300") {
     pf::Population initial_population{299, 1, 0};
-    pf::Epidemic epidemic(0.99, 0.01, initial_population);
-    int const T{15};
-    const auto state = epidemic.evolve(T);
+    pf::Epidemic epidemic(0.99, 0.01, initial_population, 15);
+    const auto state = epidemic.evolve();
     CHECK(result(state[0], {299, 1, 0}));
     CHECK(result(state[1], {298, 1, 1}));
     CHECK(result(state[2], {297, 1, 2}));
@@ -90,16 +63,46 @@ TEST_CASE("Testing the evolve function") {
 
   SUBCASE("Normal use, beta >> gamma, N=300") {
     pf::Population initial_population{299, 1, 0};
-    pf::Epidemic epidemic(0.99, 0.01, initial_population);
-    int const T{15};
-    const auto state = epidemic.evolve(T);
+    pf::Epidemic epidemic(0.99, 0.01, initial_population, 9);
+    const auto state = epidemic.evolve();
     CHECK(result(state[0], {299, 1, 0}));
     CHECK(result(state[1], {298, 2, 0}));
     CHECK(result(state[2], {296, 4, 0}));
     CHECK(result(state[3], {292, 8, 0}));
     CHECK(result(state[4], {284, 16, 0}));
-    CHECK(result(state[13], {0, 285, 15}));
-    CHECK(result(state[14], {0, 282, 18}));
-    CHECK(result(state[15], {0, 279, 21}));
+    CHECK(result(state[9], {55, 241, 4}));
+  }
+
+  SUBCASE("Beginning of Lockdown") {
+    pf::Population initial_population{299, 1, 0};
+    pf::Epidemic epidemic(0.99, 0.01, initial_population, 30);
+    const auto state = epidemic.evolve();
+    CHECK(result(state[9], {55, 241, 4}));
+    CHECK(result(state[10], {55, 239, 6}));
+    CHECK(result(state[11], {55, 237, 8}));
+    CHECK(result(state[12], {55, 235, 10}));
+    CHECK(result(state[23], {55, 213, 32}));
+    CHECK(result(state[24], {55, 211, 34}));
+    CHECK(result(state[25], {55, 209, 36}));
+  }
+
+  SUBCASE("Chages from normal use and lockdown") {
+    pf::Population initial_population{299, 1, 0};
+    pf::Epidemic epidemic(0.99, 0.01, initial_population, 60);
+    const auto state = epidemic.evolve();
+    CHECK(result(state[39], {55, 181, 64}));
+    CHECK(result(state[50], {55, 159, 86}));
+    CHECK(result(state[51], {55, 157, 88}));
+    CHECK(result(state[52], {27, 184, 89}));
+    CHECK(result(state[53], {27, 182, 91}));
+  }
+
+  SUBCASE("End of lockdown") {
+    pf::Population initial_population{299, 1, 0};
+    pf::Epidemic epidemic(0.99, 0.01, initial_population, 80);
+    const auto state = epidemic.evolve();
+    CHECK(result(state[66], {27, 156, 117}));
+    CHECK(result(state[67], {13, 168, 119}));
+    CHECK(result(state[80], {0, 157, 143}));
   }
 }
