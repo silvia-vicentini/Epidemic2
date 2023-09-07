6f380fca30e6917dc1b438e50142c3a6ebb14d8a
silvia-vicentini <143335928+silvia-vicentini@users.noreply.github.com>
2023-09-07 14:23:07 +0000

    Update functions.hpp

--- functions/functions.hpp
+++ functions/functions.hpp
@@ -1,107 +1,35 @@
-#include "functions.hpp"
-
-#include <cassert>
-#include <cmath>
+#ifndef EPIDEMIC_HPP
+#define EPIDEMIC_HPP
 #include <vector>
 
 namespace pf {
 
-Epidemic::Epidemic(double beta, double gamma,
-                   Population initial_population, long int T)
-    : m_beta(beta), m_gamma(gamma), m_initial_population(initial_population), m_T(T) {
-  assert(m_beta >= 0. && m_beta <= 1.);
-  assert(m_gamma >= 0. && m_gamma <= 1.);
-  assert(m_beta / m_gamma > 1);
-  assert(m_initial_population.S >= 0);
-  assert(m_initial_population.I >= 0);
-  assert(m_initial_population.R >= 0);
-  assert(m_T > 0);
-}
+struct Population {
+  long int S;
+  long int I;
+  long int R;
+};
 
-long int Epidemic::N() const{
-  return m_initial_population.S + m_initial_population.I +
-         m_initial_population.R;
-}
+class Epidemic {
+ private:
+  double m_beta;
+  double m_gamma;
+  Population m_initial_population;
+  long int m_T;
 
-// definition of solve function
-Population Epidemic::solve(Population prev_state) {
-  Population next_state;
-  next_state.S = std::round(prev_state.S - m_beta * prev_state.S *
-                                               prev_state.I / Epidemic::N());
-  next_state.I = std::round(
-      prev_state.I + m_beta * prev_state.S * prev_state.I / Epidemic::N() -
-      m_gamma * prev_state.I);
-  next_state.R = std::round(prev_state.R + m_gamma * prev_state.I);
-  return next_state;
-}
+  long int N() const;
 
-// definition of lockdown function
-Population Epidemic::lockdown(Population prev_state) {
-  Population next_state;
-  next_state.S = prev_state.S;
-  next_state.R = std::round(prev_state.R + m_gamma * prev_state.I);
-  next_state.I = Epidemic::N() - prev_state.S - next_state.R;
-  return next_state;
-}
-/*
-// se si fanno dei vaccini parte dei sani entra nei R perché diventano immuni
-Population vaccine(Population prev_state,  N, double beta,
-                   double gamma) {
-  Population next_state;
-  next_state.S = std::round(prev_state.S -
-                            beta * prev_state.S * prev_state.I / N - 0.05 *
-N); next_state.R = std::round(prev_state.R + gamma * prev_state.I + 0.05 * N);
-  next_state.I =
-      std::round(prev_state.I + beta * prev_state.S * prev_state.I / N -
-                 gamma * prev_state.I);
-  return next_state;
-}*/
+  Population solve(Population);
 
-Population Epidemic::approx(Population population_state) {
-  long int tot = population_state.S + population_state.I + population_state.R;
-  if (tot != Epidemic::N()) {
-    long int diff = tot - Epidemic::N();
-    if (diff > 0) {
-      population_state.R -= diff;
-    } else {
-      population_state.I -= diff;
-    }
-  }
-  return population_state;
-}
+  Population lockdown(Population);
 
-std::vector<Population> Epidemic::evolve() {
-  std::vector<Population> population_state_;
-  population_state_.push_back(m_initial_population);
-  for (long int i = 0; i < m_T;) {
-    if (population_state_[i].I < 0.6 * Epidemic::N()) {
-      Population next_state = approx(solve(population_state_[i]));
-      population_state_.push_back(next_state);
-      ++i;
-    } else {
-      if (m_T - i < 14) {
-        long int b = i;
-        for (long int a = 0; a < m_T - i; ++a) {
-          Population next_state = lockdown(population_state_[b]);
-          population_state_.push_back(next_state);
-          ++b;
-        }
-        i = b;
-      } else {
-        for (long int a = 0; a < 14; ++a) {
-          Population next_state = lockdown(population_state_[i]);
-          population_state_.push_back(next_state);
-          ++i;
-        }
-      }
-    }
-  }
-  /*else {
-    auto next_state =
-        approx(vaccine(population_state_[i], N, beta, gamma), N);
-    population_state_.push_back(next_state);
-  }
-  }*/
-  return population_state_;
-}
+  Population approx(Population);
+
+ public:
+  Epidemic(double, double, Population, long int);
+
+  std::vector<Population> evolve();
+};
 }  // namespace pf
+
+#endif
