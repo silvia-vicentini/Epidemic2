b74b8818d9d3b97f1207b36ab454e1c2a8292c80
silvia-vicentini <143335928+silvia-vicentini@users.noreply.github.com>
2023-10-20 10:49:11 +0000

    Update epidemic.cpp

--- epidemic/epidemic.cpp
+++ epidemic/epidemic.cpp
@@ -1,4 +1,4 @@
-#include "functions.hpp"
+#include "epidemic.hpp"
 
 #include <cassert>
 #include <cmath>
@@ -6,61 +6,82 @@
 
 namespace pf {
 
+// constructor
 Epidemic::Epidemic(double beta, double gamma,
-                   Population initial_population, long int T)
-    : m_beta(beta), m_gamma(gamma), m_initial_population(initial_population), m_T(T) {
-  assert(m_beta >= 0. && m_beta <= 1.);
-  assert(m_gamma >= 0. && m_gamma <= 1.);
-  assert(m_beta / m_gamma > 1);
-  assert(m_initial_population.S >= 0);
-  assert(m_initial_population.I >= 0);
-  assert(m_initial_population.R >= 0);
-  assert(m_T > 0);
+                   Population<double> initial_population, int T)
+    : beta_(beta),
+      gamma_(gamma),
+      initial_population_(initial_population),
+      T_(T) {
+  assert(beta_ >= 0. && beta_ <= 1.);
+  assert(gamma_ >= 0. && gamma_ <= 1.);
+  assert(beta_ / gamma_ > 1);
+  assert(initial_population_.S >= 0);
+  assert(initial_population_.I >= 0);
+  assert(initial_population_.R >= 0);
+  assert(T_ > 0);
+
+  std::vector<Population<double>> simulation_double_ = Epidemic::store();
 }
 
-long int Epidemic::N() const{
-  return m_initial_population.S + m_initial_population.I +
-         m_initial_population.R;
-}
-
-// definition of solve function
-Population Epidemic::solve(Population prev_state) {
-  Population next_state;
-  next_state.S = std::round(prev_state.S - m_beta * prev_state.S *
-                                               prev_state.I / Epidemic::N());
-  next_state.I = std::round(
-      prev_state.I + m_beta * prev_state.S * prev_state.I / Epidemic::N() -
-      m_gamma * prev_state.I);
-  next_state.R = std::round(prev_state.R + m_gamma * prev_state.I);
+// private methods
+Population<double> Epidemic::calculate(Population<double> prev_state) {
+  Population<double> next_state;
+  next_state.S = prev_state.S - beta_ * prev_state.S * prev_state.I / N();
+  next_state.I = prev_state.I + beta_ * prev_state.S * prev_state.I / N() -
+                 gamma_ * prev_state.I;
+  next_state.R = prev_state.R + gamma_ * prev_state.I;
   return next_state;
 }
 
-// definition of lockdown function
-Population Epidemic::lockdown(Population prev_state) {
-  Population next_state;
+Population<double> Epidemic::lockdown(Population<double> prev_state) {
+  Population<double> next_state;
   next_state.S = prev_state.S;
-  next_state.R = std::round(prev_state.R + m_gamma * prev_state.I);
-  next_state.I = Epidemic::N() - prev_state.S - next_state.R;
+  next_state.R = prev_state.R + gamma_ * prev_state.I;
+  next_state.I = N() - prev_state.S - next_state.R;
   return next_state;
 }
-/*
-// se si fanno dei vaccini parte dei sani entra nei R perché diventano immuni
-Population vaccine(Population prev_state,  N, double beta,
-                   double gamma) {
-  Population next_state;
-  next_state.S = std::round(prev_state.S -
-                            beta * prev_state.S * prev_state.I / N - 0.05 *
-N); next_state.R = std::round(prev_state.R + gamma * prev_state.I + 0.05 * N);
-  next_state.I =
-      std::round(prev_state.I + beta * prev_state.S * prev_state.I / N -
-                 gamma * prev_state.I);
-  return next_state;
-}*/
 
-Population Epidemic::approx(Population population_state) {
-  long int tot = population_state.S + population_state.I + population_state.R;
-  if (tot != Epidemic::N()) {
-    long int diff = tot - Epidemic::N();
+std::vector<Population<double>> Epidemic::store() {
+  std::vector<Population<double>> simulation_double{initial_population_};
+  for (int i = 0; i < T_;) {
+    if (simulation_double[i].I < 0.6 * N()) {
+      Population<double> next_state = calculate(simulation_double[i]);
+      simulation_double.push_back(next_state);
+      ++i;
+    } else {
+      if (T_ - i < 14) {
+        int b = i;
+        for (int a = 0; a < T_ - i; ++a) {
+          Population<double> next_state = lockdown(simulation_double[b]);
+          simulation_double.push_back(next_state);
+          ++b;
+        }
+        i = b;
+      } else {
+        for (int a = 0; a < 14; ++a) {
+          Population<double> next_state = lockdown(simulation_double[i]);
+          simulation_double.push_back(next_state);
+          ++i;
+        }
+      }
+    }
+  }
+  return simulation_double;
+}
+
+Population<int> Epidemic::round(Population<double> double_state) {
+  Population<int> int_state;
+  int_state.S = std::round(double_state.S);
+  int_state.I = std::round(double_state.I);
+  int_state.R = std::round(double_state.R);
+  return int_state;
+}
+
+Population<int> Epidemic::approx(Population<int> population_state) {
+  int tot = population_state.S + population_state.I + population_state.R;
+  if (tot != N()) {
+    int diff = tot - N();
     if (diff > 0) {
       population_state.R -= diff;
     } else {
@@ -70,38 +91,23 @@ Population Epidemic::approx(Population population_state) {
   return population_state;
 }
 
-std::vector<Population> Epidemic::evolve() {
-  std::vector<Population> population_state_;
-  population_state_.push_back(m_initial_population);
-  for (long int i = 0; i < m_T;) {
-    if (population_state_[i].I < 0.6 * Epidemic::N()) {
-      Population next_state = approx(solve(population_state_[i]));
-      population_state_.push_back(next_state);
-      ++i;
-    } else {
-      if (m_T - i < 14) {
-        long int b = i;
-        for (long int a = 0; a < m_T - i; ++a) {
-          Population next_state = lockdown(population_state_[b]);
-          population_state_.push_back(next_state);
-          ++b;
-        }
-        i = b;
-      } else {
-        for (long int a = 0; a < 14; ++a) {
-          Population next_state = lockdown(population_state_[i]);
-          population_state_.push_back(next_state);
-          ++i;
-        }
-      }
-    }
+// public methods
+std::vector<Population<int>> Epidemic::evolution() {
+  std::vector<Population<int>> simulation_int_;
+  for (auto population_state_double : simulation_double_) {
+    Population<int> population_state_int =
+        approx(round(population_state_double));
+    simulation_int_.push_back(population_state_int);
   }
-  /*else {
-    auto next_state =
-        approx(vaccine(population_state_[i], N, beta, gamma), N);
-    population_state_.push_back(next_state);
-  }
-  }*/
-  return population_state_;
+  return simulation_int_;
 }
+
+std::vector<Population<double>> Epidemic::get_simulation_double() const {
+  return simulation_double_;
+}
+int Epidemic::N() const {
+  return initial_population_.S + initial_population_.I + initial_population_.R;
+}
+int Epidemic::getT() const { return T_; }
+
 }  // namespace pf
